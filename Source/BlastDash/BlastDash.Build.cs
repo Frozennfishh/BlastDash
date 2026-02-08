6496bc7080b45efb3c6ab894a11afa5eca724b09
ArtillerySun <e1297745@u.nus.edu>
2026-02-08 22:30:35 +0800

    update template

---
+++ Source/BlastDash/BlastDash.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class BlastDash : ModuleRules
+{
+	public BlastDash(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"BlastDash",
+			"BlastDash/Variant_Horror",
+			"BlastDash/Variant_Horror/UI",
+			"BlastDash/Variant_Shooter",
+			"BlastDash/Variant_Shooter/AI",
+			"BlastDash/Variant_Shooter/UI",
+			"BlastDash/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/BlastDash/BlastDash.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BlastDash.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BlastDash, "BlastDash" );
+
+DEFINE_LOG_CATEGORY(LogBlastDash)
